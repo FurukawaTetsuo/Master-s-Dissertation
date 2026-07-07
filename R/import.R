0df0a824a745c14626f7c52c1111c3fffeb5532a
FurukawaTetsuoa <267055614+FurukawaTetsuo@users.noreply.github.com>
2026-07-07 17:55:07 +0100

    Adding revisit for 18-19

--- R/import.R
+++ R/import.R
@@ -7,7 +7,7 @@ data17_18_fv <- read.dta(here("Data", "17-18", "hh_per_fv_2017-18.dta"))
 data17_18_rv <- read.dta(here("Data", "17-18", "hh_per_rv_2017-18.dta"))
 
 data18_19_fv <- read.dta(here("Data","18-19","PerV1_2018-19.dta"))
-
+data18_19_rv <- read.dta(here("Data","18-19","PerRV_2018-19.dta"))
 
 state_district_codes <- unique(hh_17_18_fv[c("state_per_fv", "b1q4_per_fv")])
 
