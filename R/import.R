f0646f00c0f8ece79247b3492bfde1c13b6812ac
FurukawaTetsuoa <267055614+FurukawaTetsuo@users.noreply.github.com>
2026-07-07 17:58:04 +0100

    ridding things not needed for now

--- R/import.R
+++ R/import.R
@@ -9,14 +9,14 @@ data17_18_rv <- read.dta(here("Data", "17-18", "hh_per_rv_2017-18.dta"))
 data18_19_fv <- read.dta(here("Data","18-19","PerV1_2018-19.dta"))
 data18_19_rv <- read.dta(here("Data","18-19","PerRV_2018-19.dta"))
 
-state_district_codes <- unique(hh_17_18_fv[c("state_per_fv", "b1q4_per_fv")])
+#state_district_codes <- unique(data17_18_fv[c("state_per_fv", "b1q4_per_fv")])
 
 #Find num of unique state-district combinations
-nrow(state_district_codes)
+#nrow(state_district_codes)
 
-for (x in 1:nrow(state_district_codes)) {
-  state_district_codes$district[x] <- x
-}
+#for (x in 1:nrow(state_district_codes)) {
+#  state_district_codes$district[x] <- x
+#}
 
-nstates <- as.integer(unique(hh_17_18_fv$state_per_fv))
-length(nstates)
+#nstates <- as.integer(unique(data17_18_fv$state_per_fv))
+#length(nstates)
