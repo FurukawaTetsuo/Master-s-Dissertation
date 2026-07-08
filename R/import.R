7920a229e84d89847ac724a0b2901080f990a4a8
FurukawaTetsuo <267055614+FurukawaTetsuo@users.noreply.github.com>
2026-07-08 13:58:07 +0100

    linux hyprland test

--- R/import.R
+++ R/import.R
@@ -9,8 +9,6 @@ data17_18_rv <- read.dta(here("Data", "17-18", "hh_per_rv_2017-18.dta"))
 data18_19_fv <- read.dta(here("Data","18-19","PerV1_2018-19.dta"))
 data18_19_rv <- read.dta(here("Data","18-19","PerRV_2018-19.dta"))
 
-#Test it works on Linux
-
 #state_district_codes <- unique(data17_18_fv[c("state_per_fv", "b1q4_per_fv")])
 
 #Find num of unique state-district combinations
